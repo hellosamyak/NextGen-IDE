7e9eca7884f12daebda365f8495189e5890f8363
hellosamyak <jainsamyak0805@gmail.com>
2024-05-31 08:48:41 +0530

    update in NextGen

--- src/server.js
+++ src/server.js
@@ -1,26 +1,3 @@
-// const WebSocket = require('ws');
-
-// const server = new WebSocket.Server({ port: 8080 });
-
-// server.on('connection', socket => {
-//   console.log('A client connected');
-
-//   socket.on('message', message => {
-//     console.log(`Received message: ${message}`);
-//     server.clients.forEach(client => {
-//       if (client !== socket && client.readyState === WebSocket.OPEN) {
-//         client.send(message);
-//       }
-//     });
-//   });
-
-//   socket.on('close', () => {
-//     console.log('A client disconnected');
-//   });
-// });
-
-// console.log('WebSocket server running on ws://localhost:8080');
-
 const WebSocket = require('ws');
 
 const PORT = process.env.PORT || 8080;
